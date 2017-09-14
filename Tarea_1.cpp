7ba64c0b4af2642d24b4ea7a0f8a9a30c25061ea
henposolera <31905743+henposolera@users.noreply.github.com>
2017-09-14 13:17:30 -0600

    Update Tarea_1.cpp

--- Tarea_1.cpp
+++ Tarea_1.cpp
@@ -1,54 +1,51 @@
-//Tarea 1 //
-// Tarea1.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	cout << "Hello" << endl;
-	//Recibir los puntos de cada turno para su procesamiento.//
-	int turno1;
-	cout << "Turno #1" << endl;
-	cin >> turno1;
-	while (turno1 <= 0 || turno1 > 10) {
-		cout << "Error: ingrese numero correcto" << endl;
-		cout << "Turno #1" << endl;
-		cin >> turno1;
-	}
-	cout << "Turno 1 = " << turno1 << endl;
-	int turno2;
-	cout << "Turno #2" << endl;
-	cin >> turno2;
-	while (turno2 <= 0 || turno2 > 10) {
-		cout << "Error: ingrese numero correcto" << endl;
-		cout << "Turno #2" << endl;
-		cin >> turno2;
-	}
-	cout << "Turno 2 = " << turno2 << endl;
-	//int sumTurnos = turno1 + turno2;
-	//cout << "La suma de turnos es: " << sumTurnos << endl;
+int main() {
+	int participante = 0;
+	int turno1, turno2, turno3;
+	int suma[]={0,0,0};
 
-	int turno3;
-	cout << "Turno #3" << endl;
-	cin >> turno3;
-	while (turno3 <= 0 || turno3 > 10) {
-		cout << "Error: ingrese numero correcto" << endl;
-		cout << "Turno #3" << endl;
-		cin >> turno3;
+	cout << "Ingrese Numero de Participantes: " << flush;
+	cin >> participante;
+	participante = participante -1;
+	for(int i = 0;  i <= participante; i++)
+	    {
+		   cout << endl;
+		   cout << "Participante: " << (i+1) << endl;
+		   cout << "Turno 1: " << flush;
+		   cin >> turno1;
+		   while (turno1 <= 0 || turno1 > 10) {
+		   		cout << "Error: solo puede ingresar numeros del 1 al 10" << endl;
+		   		cout << "Ingrese el puntaje del Turno #1: " << flush;
+		   		cin >> turno1;
+		   	}
+		   cout << "Turno 2: " << flush;
+		   cin >> turno2;
+		   while (turno2 <= 0 || turno2 > 10) {
+		   		cout << "Error: solo puede ingresar numeros del 1 al 10" << endl;
+		   		cout << "Ingrese el puntaje del Turno #2: " << flush;
+		   		cin >> turno2;
+		   	}
+		   cout << "Turno 3: " << flush;
+		   cin >> turno3;
+		   while (turno3 <= 0 || turno3 > 10) {
+		   		cout << "Error: solo puede ingresar numeros del 1 al 10" << endl;
+		   		cout << "Ingrese el puntaje del Turno #3: " << flush;
+		   		cin >> turno3;
+		   	}
+		   suma[i] = turno1+turno2+turno3;
+		   cout << "Total participante # " << (i+1) << ": "<< suma[i] << endl;
+			   if (suma[i] >= 21) {
+					cout << "Felicidades: has ganado un almuerzo de cortesia!!!" << endl;
+				  } else {
+					cout << "Mala Suerte: no has ganado el almuerzo, vuelte a intentar pronto!!!" << "\n" << endl;
+				  }
+		   int totalPuntos = suma[0]+suma[1]+suma[2];
+		   cout << "Total de Jugadores: " << (i+1) << endl;
+		   cout << "Total de puntos en ronda: " << totalPuntos << endl;
+		   cout << "Promedio de puntos obtenidos: " << totalPuntos/(i+1) << endl;
+
+	    }
+	  return 0;
 	}
-	cout << "Turno 3 = " << turno3 << endl;
-	int sumTurnos = turno1 + turno2 + turno3;
-	cout << "La suma de turnos es: " << sumTurnos << endl;
-
-
-	/*cout << "Turno #2" << endl;
-	int turno2;
-	cin >> turno2;
-	cout << "Turno #3" << endl;
-	int turno3;
-	cin >> turno3;*/
-    return 0;
-}
